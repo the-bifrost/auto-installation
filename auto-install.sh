8b80022e3ff4fa25ba8e06b79c62ebe558b40d16
Kerlon Ribeiro <124305955+kerlonr@users.noreply.github.com>
2025-08-11 13:56:49 -0300

    Update auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -53,7 +53,7 @@ fi
 # --- Python ---
 if ! command -v python3 &>/dev/null; then
     info "Instalando Python..."
-    sudo apt install -y python3 python3-pip python3-venv
+    sudo apt install -y python3 python3-pip3 python3-venv
 else
     ok "Python já está instalado."
 fi
